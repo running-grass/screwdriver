e0e5e4945df39adff9830ddc4fca4b044f41b558
奔跑的小草 <running-grass@foxmail.com>
2020-07-14 17:38:17 +0800

    多了一个map测试

--- packages/screwdriver-test/__tests__/Linked.test.ts
+++ packages/screwdriver-test/__tests__/Linked.test.ts
@@ -1,21 +1,26 @@
-import { add, SingleLinked } from 'screwdriver-js';
+import { map, SingleLinked } from 'screwdriver-js';
 
 test("test linked", () => {
-    const l1 = SingleLinked.fromArray([2,1,4]);
+    const l1 = SingleLinked.fromArray([2, 1, 4]);
     expect(l1.size()).toBe(3);
     expect(l1.getHead().getData()).toBe(2);
 
     const l2 = l1.add(10);
     expect(l2.size()).toBe(4)
     expect(l1.size()).toBe(3);
-    expect(l2.toArray()).toEqual([2,1,4,10])
+    expect(l2.toArray()).toEqual([2, 1, 4, 10])
 
     expect(l2.getHead()).not.toBe(l1.getHead())
     expect(l2.getHead().getData()).toEqual(l1.getHead().getData())
 
 
-    const l3 = l2.map(a => a+1) as SingleLinked<number>;
-    expect(l3.toArray()).toEqual([3,2,5,11]);
+    const l3 = l2.map(a => a + 1) as SingleLinked<number>;
+    expect(l3.toArray()).toEqual([3, 2, 5, 11]);
     expect(l3.size()).toBe(4);
 
+    const mapper = function (a: number): number {
+        return a + 1;
+    };
+    const l4 = map(mapper, l3) as SingleLinked<number>
+    expect(l4.toArray()).toEqual([4, 3, 6, 12]);
 });
