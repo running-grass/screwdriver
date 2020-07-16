a9edcb179bcbcaf44c60542987a05a80a3f09a74
奔跑的小草 <running-grass@foxmail.com>
2020-07-16 19:35:46 +0800

    docs(all): 使用Typedoc搭建文档框架

--- packages/screwdriver-js/src/typeclass/Functor.ts
+++ packages/screwdriver-js/src/typeclass/Functor.ts
@@ -1,25 +1,47 @@
+/**
+ * Functor函子，最基础也是最实用的一个类型类
+ * @category 类型类
+ * @packageDocumentation
+ */
 import { Mapper } from "..";
 
+/**
+ * Common functor
+ * @template A 
+ */
 export interface CommonFunctor<A> {
   map<B>(mapper: Mapper<A, B>): Functor<B>;
 }
+
+/**
+ * Fantasy functor
+ * @template A 
+ * @category Fantasy-Land
+ */
 export interface FantasyFunctor<A> {
   'fantasy-land/map'<B>(mapper: Mapper<A, B>): Functor<B>;
 }
+
+/**
+ * Functor
+ * @template A 
+ */
 export interface Functor<A> extends
   CommonFunctor<A>,
   FantasyFunctor<A> {
 }
 
+
+/**
+ * map
+ * @param mapper 函数 a -> b
+ * @param fa 
+ */
 export function map<A, B>(mapper: Mapper<A, B>, fa: Functor<A>): Functor<B> {
   return fa.map(mapper);
 }
 
-
-
 export default Functor;
-
-
 
 // 定义接口
 declare global {
